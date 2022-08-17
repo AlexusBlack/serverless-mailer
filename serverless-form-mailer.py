f2191e81967462e4d4df55db1e1103d8a42d9add
Alex Chernov <alexusblack@gmail.com>
2022-08-17 16:30:34 +1000

    better config format, start of AWS Lambda integration

--- serverless-form-mailer.py
+++ serverless-form-mailer.py
@@ -1,23 +1,38 @@
 import smtplib
 import ssl
-import json
+# import json
+import configparser
 
-account_file_path = 'smtp_account.json'
+config = configparser.ConfigParser()
+config.read('settings.ini')
 
-with open(account_file_path) as file:
-  data = file.read()
-  account = json.loads(data)
+account = config['SMTP Account']
+settings = config['Settings']
+
+allowed_origins = settings['Allowed Origins'].strip().split("\n")
+print(allowed_origins)
+quit()
+
+# def lambda_handler(event, context):
+#   allowed_origins = ['http://localhost:3000', 'https://redsuburbs.com.au']
+#   origin = event['headers']['origin'] if 'origin' in event['headers'] else None
+#
+#   if origin != None and origin not in allowed_origins:
+#     return {
+#       'statusCode': 403,
+#       'body': json.dumps('You are not allowed to access this resource')
+#     }
 
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(account['server'], account['port'], context=context) as server:
-  server.login(account['user'], account['password'])
+with smtplib.SMTP_SSL(account['Server'], account['Port'], context=context) as server:
+  server.login(account['User'], account['Password'])
   # Send email here
   message = """\
-Subject: Test message
+Subject: Test message 2
 
-Hi Alex from python script """
+Hi Alex from python script 2"""
   server.sendmail(account['user'], 'alex@chernov.net', message)
 
 
