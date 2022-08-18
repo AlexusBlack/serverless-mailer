39afffcd0726724db4ec19f4b14f2b487ec47a2f
Alex Chernov <alexusblack@gmail.com>
2022-08-18 16:15:07 +1000

    test suite

---
+++ tests.py
@@ -0,0 +1,127 @@
+import unittest
+
+from serverless_mailer import lambda_handler
+
+class ServerlessMailerTests(unittest.TestCase):
+  def test_no_origin(self):
+    response = lambda_handler({
+      'headers': {},
+      'httpMethod': 'GET'
+    }, None)
+    self.assertEqual(response['statusCode'], 403)
+
+  def test_bad_origin(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'https://google.com' },
+      'httpMethod': 'GET'
+    }, None)
+    self.assertEqual(response['statusCode'], 403)
+
+
+  def test_good_origin(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'GET'
+    }, None)
+    self.assertNotEqual(response['statusCode'], 403)
+
+  def test_wrong_method(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'GET'
+    }, None)
+    self.assertEqual(response['statusCode'], 405)
+
+  def test_correct_method(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+    }, None)
+    self.assertNotEqual(response['statusCode'], 405)
+
+  def test_missing_body(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+    }, None)
+    self.assertEqual(response['statusCode'], 418)
+
+  def test_null_body(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': None
+    }, None)
+    self.assertEqual(response['statusCode'], 418)
+
+  def test_present_body(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{}'
+    }, None)
+    self.assertNotEqual(response['statusCode'], 418)
+
+  def test_invalid_json(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{/%'
+    }, None)
+    self.assertEqual(response['statusCode'], 400)
+    self.assertEqual(response['body'], '"Request body is not a valid JSON"')
+
+  def test_missing_subject(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{"content": "hello world"}'
+    }, None)
+    self.assertEqual(response['statusCode'], 400)
+    self.assertEqual(response['body'], '"Request body missing subject or content"')
+
+  def test_missing_content(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{"subject": "your extended warranty"}'
+    }, None)
+    self.assertEqual(response['statusCode'], 400)
+    self.assertEqual(response['body'], '"Request body missing subject or content"')
+
+  def test_correct_content(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{"subject": "your extended warranty", "content": "hello world"}'
+    }, None)
+    self.assertNotEqual(response['statusCode'], 400)
+
+  def test_bad_recipent(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{"to": "bill@microsoft.com", "subject": "your extended warranty","content": "hello world"}'
+    }, None)
+    self.assertEqual(response['statusCode'], 400)
+    self.assertEqual(response['body'], '"The recipient is not allowed"')
+
+  def test_good_recipent(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{"to": "test@email.com", "subject": "your extended warranty","content": "hello world"}'
+    }, None)
+    self.assertNotEqual(response['statusCode'], 400)
+
+  def test_correct_send_response(self):
+    response = lambda_handler({
+      'headers': { 'origin': 'http://localhost:3000' },
+      'httpMethod': 'POST',
+      'body': '{"to": "test@email.com", "subject": "your extended warranty","content": "hello world"}'
+    }, None)
+    self.assertEqual(response['statusCode'], 200)
+    self.assertEqual(response['headers']['Access-Control-Allow-Origin'], 'http://localhost:3000')
+
+if __name__ == '__main__':
+  unittest.main()
